e782790cc37b21a5bc46d839147b57a22b2f2167
Ruffle <noruffle@gmail.com>
2023-03-12 02:56:02 +0100

    models

--- database/models/user.model.ts
+++ database/models/user.model.ts
@@ -1,65 +1,11 @@
-import {UserInterface} from '../../interfaces/user.interface';
-import mongoose from 'mongoose';
-
-
+import mongoose, { InferSchemaType, model } from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-  // Register 
-  important: {
-    type: Object,
-
-    email: String,
-    password: String,
-    dob: Function
-  },
-  // up to update
-  info: {
-    type: Object,
-
-    nickname: String,
-    avatar: String,
-    fullName: {
-      type: Object,
-
-      firstName: String,
-      lastName: String
-    },
-    gender: String,
-    location: {
-      type: Object,
-      
-      county: String,
-      city: String
-    },
-    socialMedia: String
-
-  }
-
+    username: {type: String, required: true, unique: true},
+    email: {type: String, required: true, unique: true, select: false},
+    password: {type: String, required: true, select: false},
 })
 
+type User = InferSchemaType<typeof userSchema>
 
-
-export default mongoose.model('User', userSchema);
-
-
-/**
- * User
- * 
- * registration { method: post
- * email
- * password
- * }
- * 
- * update user { method: patch
- * nickname (login)
- * photo
- * 
- * fullname: first name, last name
- * about
- * gender: male, female, other
- * date of birth
- * location: country, city
- * social medias: vk, twitter, inst, twitch, discord, telegram
- * }
- * 
- */
+export default model<User>("User", userSchema)
